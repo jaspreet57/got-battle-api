9825c68cb280b0faf86748231172a4886023756b
Jaspreet Singh <erjaspreet57@gmail.com>
2017-07-18 13:14:35 +0530

    updating for heroku production

--- server.js
+++ server.js
@@ -1,29 +1,38 @@
-import express from 'express';
-import morgan from 'morgan';
+'use strict';
 
-//import api end points
-import { getList, getCount, getStats, searchBattles } from './routes/battles';
+var _express = require('express');
 
+var _express2 = _interopRequireDefault(_express);
+
+var _morgan = require('morgan');
+
+var _morgan2 = _interopRequireDefault(_morgan);
+
+var _battles = require('./routes/battles');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 //Set up app and use morgan logger
-const app = express();
-app.use(morgan('dev'));
+var app = (0, _express2.default)();
 
+//import api end points
+
+app.use((0, _morgan2.default)('dev'));
 
 // API routes
-app.route('/list').get(getList);
-app.route('/count').get(getCount);
-app.route('/stats').get(getStats);
-app.route('/search').get(searchBattles);
-
+app.route('/list').get(_battles.getList);
+app.route('/count').get(_battles.getCount);
+app.route('/stats').get(_battles.getStats);
+app.route('/search').get(_battles.searchBattles);
 
 // ...For all the other requests just send error message
-app.route("*").get((req, res) => {
-	var err = new Error('Not found');
-	res.status(404);
-	res.send(err);
+app.route("*").get(function (req, res) {
+        var err = new Error('Not found');
+        res.status(404);
+        res.send(err);
 });
 
 app.listen(8080);
 
 console.log('listening on port 8080');
+
